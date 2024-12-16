5d9812ff12a7d271777fb2dc7a270feba68989cf
gemaprog <gema.garciafernandez@educa.madrid.org>
2024-12-16 09:36:22 +0100

    Primer proyecto POO

---
+++ POOEj1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Mono mono1 = new Mono("Copito",18,100,false,"Gorila",20,"Luna");
+        Mono mono2 = new Mono("Amedio",3,8,false,"Chimpancé",3,"Marco");
+        Mono mono3 = new Mono();
+        System.out.println(mono1.getCuidador());
+        System.out.println(mono2.getCuidador());
+        System.out.println(mono3.getCuidador());
+        System.out.println(mono3);
+    }
+}
