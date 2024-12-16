8235dd68216e34311e13322356b18dc6d9e67d7b
gemaprog <gema.garciafernandez@educa.madrid.org>
2024-12-16 09:47:12 +0100

    Primer proyecto POO

---
+++ POOEj1/src/EntradaSalida.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EntradaSalida {
+    public static Mono dameMono(){
+        Mono nuevoMono = null;
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introduce: 1. mono estándar 2. mono personalizado");
+        int opcion = lector.nextInt();
+        if (opcion==1)
+            nuevoMono = new Mono();
+        else {
+            System.out.println("Introduce nombre, edad, peso, true (hembra) / false (varón), tipo, kilos comida semana y cuidador");
+            nuevoMono = new Mono(lector.nextLine(), lector.nextInt(), lector.nextFloat(), lector.nextBoolean(), lector.nextLine(), lector.nextFloat(), lector.nextLine());
+        }
+        return nuevoMono;
+    }
+}
--- POOEj1/src/Main.java
+++ POOEj1/src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
         Mono mono1 = new Mono("Copito",18,100,false,"Gorila",20,"Luna");
         Mono mono2 = new Mono("Amedio",3,8,false,"Chimpancé",3,"Marco");
