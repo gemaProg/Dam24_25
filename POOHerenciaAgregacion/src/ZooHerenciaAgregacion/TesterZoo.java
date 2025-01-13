902e65bcdaa34af70716595c0813757afc2d8547
gemaprog <gema.garciafernandez@educa.madrid.org>
2025-01-13 11:00:12 +0100

    Proyecto utilizar datos aleatorios con una librería DataFaker. Ojo crear el proyecto Maven

--- POOHerenciaAgregacion/src/ZooHerenciaAgregacion/TesterZoo.java
+++ POOHerenciaAgregacion/src/ZooHerenciaAgregacion/TesterZoo.java
@@ -4,9 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TesterZoo {
-    // Este método muestra el array de animales
-    // Este método gestiona el menú y determina la opción elegida
-    // llamando a otros métodos que resuelven las opciones
+
     public static void gestionMenu() {
         Scanner lector = new Scanner(System.in);
         Zoo zoo = new Zoo();
@@ -18,39 +16,38 @@ public class TesterZoo {
             System.out.println();
             System.out.println("Menú de opciones");
             System.out.println("****************");
-            System.out.println("1. Mostrar el array");
-            System.out.println("2. Buscar la posición del primer mono");
-            System.out.println("3. Contar el número de Elefantes");
-            System.out.println("4. Modificar el cuidador del elemento que digamos");
+            System.out.println("G1. Mostrar el array");
+            System.out.println("G2. Agregar un animal en el zoo");
+            System.out.println("G3. Eliminar un animal del zoo");
+            System.out.println("4. Buscar la posición del primer mono");
+            System.out.println("5. Contar el número de Elefantes");
+            System.out.println("G6. Modificar el cuidador del elemento que digamos");
+            System.out.println("G7. Modificar ornitorrinco pico");
             System.out.println("0. Salir");
 
             opcion = lector.nextInt();
             switch (opcion) {
                 case 1:
-                    // Aquí delego en un método directamente
                     zoo.mostrarArrrayDeAnimales();
                     break;
-
                 case 2:
-                    // Aquí delego en un método para obtener la posición
-                    // y el resto de la lógica la hago en local
+
+                    break;
+                case 3:
+                    break;
+                case 4:
                     aux = zoo.buscarPrimerMono();
                     if (aux > 0)
                         System.out.println("El primero mono está en la posición " + aux);
                     else
                         System.out.println("No hay monos en la lista");
                     break;
-
-                case 3:
-                    // Aquí delego en un método para obtener la posición
-                    // y el resto de la lógica la hago en local
+                case 5:
                     aux = zoo.contarElefantes();
                     System.out.println("En la lista de animales hay " + aux + " elefantes");
                     break;
 
-                case 4:
-                    // Este proceso lo voy a hacer "a la antigua", sin
-                    // llamar a un procedimiento
+                case 6:
                     System.out.println("Indique la posición del animal (0-"+(zoo.getLista().length-1)+"): ");
                     aux= lector.nextInt();
                     lector.nextLine();
@@ -68,13 +65,8 @@ public class TesterZoo {
                     break;
 
                 case 0:
-                    // La opción de salida se contempla sin código para
-                    // que no se de un mensaje de opción incorrecta
                     break;
-
                 default:
-                    // Por defecto la opción es incorrecta si no es
-                    // ninguna de las elegidas antes
                     System.out.println("Opción incorrecta");
                     break;
             }
@@ -83,7 +75,6 @@ public class TesterZoo {
 
     // Código principal del tester
     public static void main(String[] args) {
-
         gestionMenu();
     }
 }
